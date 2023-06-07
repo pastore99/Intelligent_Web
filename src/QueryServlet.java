b963145fe62afb54a9820db9ae8c233e99653b08
Carmine <c.pastore35@studenti.unisa.it>
2023-06-07 15:38:37 +0200

    servlet

---
+++ src/QueryServlet.java
@@ -0,0 +1,33 @@
+import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.rdf.model.Model;
+import de.fuberlin.wiwiss.d2rq.jena.ModelD2RQ;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "QueryServlet", value = "/QueryServlet")
+public class QueryServlet extends HttpServlet {
+    Model m = new ModelD2RQ("outfile.ttl");
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String querySparqlString = req.getParameter("querySparql");
+        System.out.println(querySparqlString);
+        Query q = QueryFactory.create(querySparqlString);
+        ResultSet rs = QueryExecutionFactory.create(q, m).execSelect();
+        while (rs.hasNext()){
+            QuerySolution row = rs.nextSolution();
+            System.out.println(row);
+        }
+        m.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
--- web/index.jsp
+++ web/index.jsp
@@ -1,8 +1,8 @@
 <%--
   Created by IntelliJ IDEA.
-  User: rocco
+  User: Carmine
   Date: 07/06/2023
-  Time: 14:37
+  Time: 15:10
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -11,6 +11,10 @@
     <title>Title</title>
 </head>
 <body>
-
+<form action="QueryServlet" method="post">
+    <textarea name="querySparql" rows="5" cols="40"></textarea>
+    <br>
+    <input type="submit" value="Invia">
+</form>
 </body>
 </html>
